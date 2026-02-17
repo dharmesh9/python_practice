acadfbb6117aa9e57747a37c779d17e9d7bf4e8e
Dharmesh Makwana <108424544+dharmesh9@users.noreply.github.com>
2026-02-17 22:34:22 +0000

    Add files via upload
    Typecasting practice file.

---
+++ 03_typecasting.py
@@ -0,0 +1,26 @@
+number1 = 9
+string1 = "9999"
+boolean = True
+float1 = 9.2222
+
+print(number1)
+print(string1)
+print(float1)
+
+# converting datatypes
+
+int_to_string = str(number1)
+print(int_to_string)
+print(type(int_to_string))
+
+string_to_integer = int(string1)
+print(string_to_integer)
+print(type(string_to_integer))
+
+float_to_string = str(float1)
+print(float_to_string)
+print(type(float1))
+
+float_to_integer = int(float1)
+print(float_to_integer)
+print(type(float_to_integer))
