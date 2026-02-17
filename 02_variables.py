5540f279dc891c6a5a5ab38c618aace98a64011c
Dharmesh Makwana <108424544+dharmesh9@users.noreply.github.com>
2026-02-17 22:32:56 +0000

    Add files via upload
    Basic syntax and variables practice with typecasting.

---
+++ 02_variables.py
@@ -0,0 +1,17 @@
+# variables
+
+user_name = "Dharmesh" # text
+user_number = 9 # iteger
+cgpa = 9.22 # float    
+is_user_available = True
+
+print(user_name)
+print(user_number)
+print(cgpa)
+print(is_user_available)
+
+
+print(type(user_name))
+print(type(user_number))
+print(type(cgpa))
+print(type(is_user_available))
