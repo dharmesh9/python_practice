887b23627eb2d1958e5320b0851c5f10e4057b52
Dharmesh Makwana <108424544+dharmesh9@users.noreply.github.com>
2026-02-18 15:00:35 +0000

    Add files via upload
    Comment, Escape Sequences & Print Statement practice.

---
+++ 05_Comment_Escape_Sequences&Print_Statement.py
@@ -0,0 +1,23 @@
+# Single line comment.
+
+'''
+this is multiline comment,
+one
+two
+three
+for
+five
+six
+seven
+eight
+nine
+'''
+
+
+
+# escape_sequaences
+print('This is \nNEW line escape_sequaence.')
+print('This is tab \t escape_sequaence.')
+print('This is backlash \\ escape_sequaence.')
+print('This is double quote \" escape_sequaence.')
+print('This is single \' escape_sequaence.')
