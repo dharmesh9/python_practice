b22cb29a3ae53cd6a9180914ce7363fc2cfe2b0a
Dharmesh Makwana <108424544+dharmesh9@users.noreply.github.com>
2026-02-18 15:06:47 +0000

    Add files via upload
    Arithmetic and conditional operators practice.

---
+++ 06_arithmetic_operators.py
@@ -0,0 +1,13 @@
+a = 999
+
+b = 9
+
+# Arithmetic Operator 
+print("a + b = ", a + b)
+print("a - b = ", a - b)
+print("a * b = ", a * b)
+print("a / b = ", a / b)
+print("a % b = ", a % b)
+print("a // b = ", a // b)
+print("a ** b = ", a ** b)
+ 
