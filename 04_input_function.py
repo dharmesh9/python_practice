4e62791af5123308865838be45eac7470b041403
Dharmesh Makwana <108424544+dharmesh9@users.noreply.github.com>
2026-02-18 14:33:37 +0000

    Add files via upload
    Input Function and F-string practice.

---
+++ 04_input_function.py
@@ -0,0 +1,26 @@
+input_function = input("Enter your name: ")
+
+print(input_function)
+
+
+# entered integer value is in string
+input_function2 = input("Enter your number: ")
+
+print(input_function2)
+
+print(type(input_function2))
+
+
+# taking input value as integer
+input_function3 = int(input("Enter your number: "))
+
+print(input_function3)
+
+print(type(input_function3))
+
+
+input_function4 = int(input("Enter your number: "))
+
+print(input_function4)
+
+print(f"Your entered number is: {input_function4}")
