936ddd1f01d677df550a1ee05421c518a6ce1621
Dharmesh Makwana <108424544+dharmesh9@users.noreply.github.com>
2026-02-20 20:30:52 +0000

    While loop practice file.

---
+++ 13_while_loop.py
@@ -0,0 +1,25 @@
+'''
+while condition:
+     Code to execute while condition is True
+'''
+
+count1 = 0
+
+print('Does not Skip Zero')
+while count1 <= 9:
+     print(count1)
+     count1 += 1
+
+
+
+count2 = 0
+print('Skips Zero')
+while count2 < 9:
+    count2 += 1
+    print(count2)
+
+'''
+while True:
+    print("This will run forever!")
+'''
+
