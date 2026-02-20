18fa89c35a738454f2a04d4d5c709a8cf951ee7f
Dharmesh Makwana <108424544+dharmesh9@users.noreply.github.com>
2026-02-20 20:30:15 +0000

    For loop practice file.

---
+++ 12_for_loop.py
@@ -0,0 +1,36 @@
+'''
+syntax:
+
+for item in sequence:
+     Code to execute for each item
+
+'''
+
+list1  = ['a','b','c','d']
+
+for item in list1:
+    print(item)
+
+
+for item_number in range(9):
+    print(item_number)
+
+for item_number in range(1,10):
+    print(item_number)
+
+
+for item_number in range(1,11):
+    print('9 *',item_number, '=' ,9 * (item_number))
+
+
+
+item_list = ['apple','banana','grapes','oranges']
+#prints whole list
+for items in item_list:
+    print(item_list)
+
+
+#prints list items
+for items in item_list:
+    print(items)
+
